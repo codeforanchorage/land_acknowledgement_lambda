34e4785e252bfaa6fdf0c1c9147eda3cb2acefcf
Mark Meyer <mark@photo-mark.com>
2022-03-22 17:26:40 -0800

    break out aws_secret code into module

--- chalicelib/geocode.py
+++ chalicelib/geocode.py
@@ -3,74 +3,15 @@ import json
 
 from chalicelib.errors import APIError, LocationNotFound, ConfigurationError
 from chalicelib.http import session
-
-# Use this code snippet in your app.
-# If you need more information about configurations or implementing the sample code, visit the AWS docs:   
-# https://aws.amazon.com/developers/getting-started/python/
-
-import boto3
-import base64
-from botocore.exceptions import ClientError
-
-
-def get_secret():
-
-    secret_name = "MAPBOX_TOKEN"
-    region_name = "us-west-2"
-
-    session = boto3.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-    else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        secret = json.loads(get_secret_value_response['SecretString'])
-        return secret.get('MAPBOX_TOKEN')
-            
+from chalicelib.get_secret import get_secret
 
 
 API_BASE = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
 
-MAPBOX_TOKEN = os.getenv('MAPBOX_TOKEN') or get_secret()
+MAPBOX_TOKEN = os.getenv('MAPBOX_TOKEN') or get_secret('MAPBOX_TOKEN')
 
 if MAPBOX_TOKEN is None:
     raise ConfigurationError("The env MAPBOX_TOKEN is missing from the environment.")
-
-'''
-Things that can go wrong with user input:
-Blank input — check before querrying
-Too short - can a query shorted than three characters ever be meaningful
-Too broad - 'Canada' can't provide a meaningful result
-'''
-
 
 def geolocate(raw_str):
     query = {
---
+++ chalicelib/get_secret.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret(secret_name, region_name="us-west-2"):
+
+    session = boto3.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS key.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        secret = json.loads(get_secret_value_response['SecretString'])
+        return secret.get(secret_name)
