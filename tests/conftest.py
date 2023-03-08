67c0d8aabf4d0501207d9621088d73b6f76493b4
britt-monroe <82415797+britt-monroe@users.noreply.github.com>
2023-03-07 19:08:58 -0800

    Update conftest.py to have faux credentials.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import pytest
 
@@ -11,6 +12,17 @@ class Response:
 
     def json(self):
         return self.data
+
+
+@pytest.fixture()
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["MAPBOX_TOKEN"] = "testing"
+
 
 @pytest.fixture()
 def FakeResp():
